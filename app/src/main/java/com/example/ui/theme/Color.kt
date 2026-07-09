e77efbcee6e59e00a72f5b85f7be00cc03821ed0
realbiplobhossen-lab <real.biplob.hossen@gmail.com>
2026-07-10 00:59:22 +0600

    Create Color.kt

---
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LegalNavy = Color(0xFF1E293B)
+val SlateDark = Color(0xFF0F172A)
+val SlateLight = Color(0xFFF8FAFC)
+val JudicialGold = Color(0xFFD97706)
+val GoldAccent = Color(0xFFF59E0B)
+
+val PrimaryDark = Color(0xFF38BDF8)
+val SecondaryDark = Color(0xFF94A3B8)
+val TertiaryDark = Color(0xFFF59E0B)
+
+val PrimaryLight = Color(0xFF0284C7)
+val SecondaryLight = Color(0xFF475569)
+val TertiaryLight = Color(0xFFD97706)
+
